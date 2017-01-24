621ca1b9bd089b9d9243be17ec021a89bd82feb1
josephdeeb <josephdbesenski@gmail.com>
2017-01-24 14:50:55 -0700

    Add files via upload

---
+++ Planning/Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class Frame extends JFrame {
+    
+    public Frame() {
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setSize(200, 200);
+
+    }
+}
