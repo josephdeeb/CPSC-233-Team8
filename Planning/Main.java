e0c31dbc433669646aa0fcc04f4437814532c59d
josephdeeb <josephdbesenski@gmail.com>
2017-01-26 14:51:15 -0700

    Add files via upload

--- Planning/Main.java
+++ Planning/Main.java
@@ -1,3 +1,12 @@
+/*
+ * CURRENT PROBLEMS
+ * 
+ * window and game cannot be referenced in the main method (possibly in other methods as well)
+ * 
+ */
+
+
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -7,6 +16,8 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.Color;
 import javax.swing.BorderFactory;
+
+import java.lang.Thread;
 
 public class Main {
     
@@ -20,12 +31,21 @@ public class Main {
     public static void main(String[] args) {
 
         initializeGame(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_NAME);
+        // You can also easily change the color
+        
+        stop(100);
+        labels[7][25].setBackground(Color.GREEN);
+        labels[7][25].setBorder(null);
+        stop(100);
+        labels[8][25].setBackground(Color.GREEN);
+        labels[8][25].setBorder(null);
+        stop(100);
+        labels[9][25].setBorder(null);
+        labels[9][25].setBackground(Color.GREEN);
     }
     
     public static void initializeGame(int w, int h, String name) {
         
-
-        // JFrame window
         Window window = new Window(w,h,name);
         // JPanel where the game takes place
         JPanel game = new JPanel(new GridLayout(50,50));
@@ -47,11 +67,31 @@ public class Main {
         
         // Just here to show how easily you can change the values or image or color of a specific square
         labels[2][10].setBackground(Color.GREEN);
-        // Makes the game JPanel visible
+
         game.setVisible(true);
         // Adds the game JPanel to the main window
         window.getContentPane().add(game);
-        // Makes the window visible
         window.setVisible(true);
+        
+        
+        // Quick test animation
+        int f = 0;
+        while (f < 50) {
+            stop(100);
+            window.repaint();
+            labels[f][25].setBackground(Color.GREEN);
+            labels[f][25].setBorder(null);
+            f++;
+        }
+    }
+    
+    // This method uses code copied from http://stackoverflow.com/questions/24104313/how-to-delay-in-java by Ann Ragg
+    public static void stop(int milliseconds) {
+        
+        try {
+            Thread.sleep(milliseconds);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
