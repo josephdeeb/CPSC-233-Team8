7b2c85293cf025881cbe49abe0e1bd41e42a86a2
DanSohn <chiefgustavo000@gmail.com>
2017-02-13 10:47:53 -0700

    Added box around the "GAME OVER"

--- Planning/Main.java
+++ Planning/Main.java
@@ -71,6 +71,7 @@ public class Main {
             game.repaint();
             stop(100);
         }
+		drawBox(11,15);
         game(13, 17);
         over(13, 25);
         
@@ -91,59 +92,66 @@ public class Main {
             }
         }
     }
+	
+	public static void drawBox(int x, int y){
+		//starting x: 11 starting y: 15
+		for(int a=y;a<34;a++){
+			drawLine(x,x+26,a,a, Color.WHITE);
+		}
+	}
     
     public static void game(int x, int y) {
         
         // G
-        drawLine(x+1, x+3, y, y, Color.YELLOW);
-        drawLine(x, x, y+1, y+5, Color.YELLOW);
-        drawLine(x+1, x+4, y+6, y+6, Color.YELLOW);
-        drawLine(x+4, x+4, y+3, y+5, Color.YELLOW);
-        drawLine(x+2, x+4, y+3, y+3, Color.YELLOW);
-        labels[x+4][y+1].colorUpdate(Color.YELLOW);
+        drawLine(x+1, x+3, y, y, Color.BLACK);
+        drawLine(x, x, y+1, y+5, Color.BLACK);
+        drawLine(x+1, x+4, y+6, y+6, Color.BLACK);
+        drawLine(x+4, x+4, y+3, y+5, Color.BLACK);
+        drawLine(x+2, x+4, y+3, y+3, Color.BLACK);
+        labels[x+4][y+1].colorUpdate(Color.BLACK);
         // A
-        drawLine(x+6, x+6, y+1, y+6, Color.YELLOW);
-        drawLine(x+10, x+10, y+1, y+6, Color.YELLOW);
-        drawLine(x+7, x+9, y, y, Color.YELLOW);
-        drawLine(x+7, x+9, y+3, y+3, Color.YELLOW);
+        drawLine(x+6, x+6, y+1, y+6, Color.BLACK);
+        drawLine(x+10, x+10, y+1, y+6, Color.BLACK);
+        drawLine(x+7, x+9, y, y, Color.BLACK);
+        drawLine(x+7, x+9, y+3, y+3, Color.BLACK);
         // M
-        drawLine(x+12, x+12, y, y+6, Color.YELLOW);
-        drawLine(x+16, x+16, y, y+6, Color.YELLOW);
-        drawLine(x+14, x+14, y+1, y+3, Color.YELLOW);
-        labels[x+13][y].colorUpdate(Color.YELLOW);
-        labels[x+15][y].colorUpdate(Color.YELLOW);
+        drawLine(x+12, x+12, y, y+6, Color.BLACK);
+        drawLine(x+16, x+16, y, y+6, Color.BLACK);
+        drawLine(x+14, x+14, y+1, y+3, Color.BLACK);
+        labels[x+13][y].colorUpdate(Color.BLACK);
+        labels[x+15][y].colorUpdate(Color.BLACK);
         // E
-        drawLine(x+18, x+18, y, y+6, Color.YELLOW);
-        drawLine(x+18, x+22, y, y, Color.YELLOW);
-        drawLine(x+18, x+21, y+3, y+3, Color.YELLOW);
-        drawLine(x+18, x+22, y+6, y+6, Color.YELLOW);
+        drawLine(x+18, x+18, y, y+6, Color.BLACK);
+        drawLine(x+18, x+22, y, y, Color.BLACK);
+        drawLine(x+18, x+21, y+3, y+3, Color.BLACK);
+        drawLine(x+18, x+22, y+6, y+6, Color.BLACK);
     }
     
     public static void over(int x, int y) {
         // O
-        drawLine(x+1, x+3, y, y, Color.YELLOW);
-        drawLine(x, x, y+1, y+5, Color.YELLOW);
-        drawLine(x+1, x+3, y+6, y+6, Color.YELLOW);
-        drawLine(x+4, x+4, y+1, y+5, Color.YELLOW);
+        drawLine(x+1, x+3, y, y, Color.BLACK);
+        drawLine(x, x, y+1, y+5, Color.BLACK);
+        drawLine(x+1, x+3, y+6, y+6, Color.BLACK);
+        drawLine(x+4, x+4, y+1, y+5, Color.BLACK);
         // V
-        drawLine(x+6, x+6, y, y+4, Color.YELLOW);
-        drawLine(x+10, x+10, y, y+4, Color.YELLOW);
-        labels[x+7][y+5].colorUpdate(Color.YELLOW);
-        labels[x+8][y+6].colorUpdate(Color.YELLOW);
-        labels[x+9][y+5].colorUpdate(Color.YELLOW);
+        drawLine(x+6, x+6, y, y+4, Color.BLACK);
+        drawLine(x+10, x+10, y, y+4, Color.BLACK);
+        labels[x+7][y+5].colorUpdate(Color.BLACK);
+        labels[x+8][y+6].colorUpdate(Color.BLACK);
+        labels[x+9][y+5].colorUpdate(Color.BLACK);
         // E
-        drawLine(x+12, x+12, y, y+6, Color.YELLOW);
-        drawLine(x+12, x+16, y, y, Color.YELLOW);
-        drawLine(x+12, x+15, y+3, y+3, Color.YELLOW);
-        drawLine(x+12, x+16, y+6, y+6, Color.YELLOW);
+        drawLine(x+12, x+12, y, y+6, Color.BLACK);
+        drawLine(x+12, x+16, y, y, Color.BLACK);
+        drawLine(x+12, x+15, y+3, y+3, Color.BLACK);
+        drawLine(x+12, x+16, y+6, y+6, Color.BLACK);
         // R
-        drawLine(x+18, x+18, y, y+6, Color.YELLOW);
-        drawLine(x+18, x+21, y, y, Color.YELLOW);
-        drawLine(x+18, x+21, y+3, y+3, Color.YELLOW);
-        drawLine(x+22, x+22, y+1, y+2, Color.YELLOW);
-        labels[x+20][y+4].colorUpdate(Color.YELLOW);
-        labels[x+21][y+5].colorUpdate(Color.YELLOW);
-        labels[x+22][y+6].colorUpdate(Color.YELLOW);
+        drawLine(x+18, x+18, y, y+6, Color.BLACK);
+        drawLine(x+18, x+21, y, y, Color.BLACK);
+        drawLine(x+18, x+21, y+3, y+3, Color.BLACK);
+        drawLine(x+22, x+22, y+1, y+2, Color.BLACK);
+        labels[x+20][y+4].colorUpdate(Color.BLACK);
+        labels[x+21][y+5].colorUpdate(Color.BLACK);
+        labels[x+22][y+6].colorUpdate(Color.BLACK);
     }
     
     public static void stopGame() {
