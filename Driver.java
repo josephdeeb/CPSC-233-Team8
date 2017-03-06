e184cddb027d40b6958e512cfd568d0cd6644573
jasonlyster <lysterjason@gmail.com>
2017-03-06 11:10:29 -0700

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -11,31 +11,58 @@ public class Driver {
     private String oldDirection = "";
     private Color playerColor = Color.YELLOW;
     
+    /**
+     * Initializes player one
+     *
+     * @param x - X Coordinate of the wanted cell
+     * @param y - Y Coordinate of the wanted cell
+     * @param color - Color of the player
+     */
     public Driver(int x, int y, Color color) {
         this(x, y, color, DEFAULT_DIRECTION);
     }
     
-    public Driver(int x, int y, Color color, String initDirection) {
+    /** 
+     * Initializes any extra players past player one
+     *
+     * @param x - X Coordinate of the wanted cell
+     * @param y - Y Coordinate of the wanted cell
+     * @param color - Color of the player
+     * @param initDirection - Initial Direction of the other players
+     */
+     public Driver(int x, int y, Color color, String initDirection) {
         xCoordinate = x;
         yCoordinate = y;
         playerColor = color;
         direction = initDirection;
     }
     
-    
+    /**
+     * Starts the game by adding player color to cells
+     */
     public void start() {
         
         Main.getCell(xCoordinate, yCoordinate).colorUpdate(playerColor);
     }
     
+    /**
+     * Getter for the X Coordinate
+     */
     public int getXCoordinate() {
         return xCoordinate;
     }
     
+    /* *
+     * Getter for the Y Coordinate
+     */
     public int getYCoordinate() {
         return yCoordinate;
     }
     
+    /**
+     * Changes the X and Y coordinates based on which direction
+     * the player chooses to move
+     */
     public void move() {
         
         int newX = 0, newY = 0;
@@ -73,6 +100,9 @@ public class Driver {
         
         Color nextColor = Color.YELLOW;
         
+        /**
+         * Tries to see if the next cell is available to move into
+         */
         try {
             
             nextColor = Main.getCell(newX, newY).getColor();
@@ -80,6 +110,10 @@ public class Driver {
         catch (Exception IndexOutOfBounds) {}
         finally {}
         
+        /** 
+         * If the next cell is availabe to move into then the cell is updated
+         * to show player movement
+         */
         if (nextColor.equals(Color.BLACK)) {
             xCoordinate = newX;
             yCoordinate = newY;
@@ -91,19 +125,36 @@ public class Driver {
             alive = false;
         }
     }
-    public boolean getAlive() {
+    
+    /**
+     * Getter for if the player is alive
+     */
+     public boolean getAlive() {
         return alive;
     }
     
+    /** 
+     * Setter for the direction
+     *
+     * @param setter- Sets the direction
+     */
     public void setDirection(String setter) {
         if ((setter == "up") || (setter == "down") || (setter == "left") || (setter == "right"))
             direction = setter;
     }
     
+    /**
+     * Getter for the direction
+     */
     public String getDirection() {
         return direction;
     }
     
+    /** 
+     * Makes sure that the player cannot move in the opposite direction
+     *
+     * @param dir - Current direction of the player
+     */
     private static String reverseDirection(String dir) {
         String reversed = "";
         
