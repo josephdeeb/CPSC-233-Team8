7225b18b779afcb66abe919a66f29d3d6f42201a
DenisShev <shevdenis1835@gmail.com>
2017-03-24 13:14:35 -0600

    Add files via upload

---
+++ GAME/graphics/ButtonCreator.java
@@ -0,0 +1,40 @@
+package graphics;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+
+
+public class ButtonCreator extends JButton{
+    /**
+     * Creates a button.
+     *
+     * @param xCoordinate Gets the x of the button
+     * @param yCoordinate Gets the y of the button
+     * @param width Determines how wide the button is
+     * @param height Determines how high the button is
+     * @param color Sets the color of the foreground of the button
+     * @param text Sets the name of the button
+     */
+	
+    public ButtonCreator(int xCoordinate, int yCoordinate, int width, int height, Color color, String text){
+        Font font;
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, new File("Tron.ttf"));
+			font = font.deriveFont(Font.PLAIN, 50);
+		} catch (FontFormatException | IOException error) {
+			font = new Font("Arial", Font.BOLD, 40);
+		}
+        setFont(font);
+        setBackground(Color.BLACK);
+        setForeground(color);
+        setBounds(xCoordinate, yCoordinate, width, height);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setVisible(true);
+        setText(text);
+    }
+    
+}
