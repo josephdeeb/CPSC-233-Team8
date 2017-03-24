3655749b51188bcdbf2b80f81abb877c28b9ed50
DenisShev <shevdenis1835@gmail.com>
2017-03-24 12:36:47 -0600

    Add files via upload

---
+++ ra/graphics/ButtonCreator.java
@@ -0,0 +1,30 @@
+package graphics;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.*;
+
+
+public class ButtonCreator extends JButton{
+	
+    public ButtonCreator(int xCoordinate, int yCoordinate, int width, int height, Color color, String text){
+        Font font;
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, new File("Tron.ttf"));
+			font = font.deriveFont(Font.PLAIN, 50);
+		} catch (FontFormatException | IOException error) {
+			font = new Font("Arial", Font.BOLD, 40);
+		}
+        setFont(font);
+        setBackground(Color.BLACK);
+        setForeground(color);
+        setBounds(xCoordinate, yCoordinate, width, height);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setVisible(true);
+        setText(text);
+    }
+    
+}
---
+++ ra/other/Settings.java
@@ -0,0 +1,188 @@
+package other;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import graphics.ButtonCreator;
+import graphics.Window;
+
+import java.awt.event.*;
+
+public class Settings {
+    
+    private static boolean boostBool = true;
+    private static int numOfPlayers = 2;
+    private static int numOfPlayersText = 1;
+    private static int difficultySpeed = 100;
+    private static boolean gameGoing = false;
+    private static boolean timer = true;
+    private static StartMenu startMenu = new StartMenu();
+    
+    /** 
+     * Runs the Settings menu
+     *
+     * @param width Sets the width of the window
+     * @param height Sets the height of the window
+     * @param name Sets the name of the window
+     */
+    public static void run(int w,int h, String name){
+        
+        // Creates a new window
+        Window frame = new Window(w, h, name);
+        
+        // Creates buttons
+        ButtonCreator start = new ButtonCreator(300, 250, 300, 100, Color.CYAN, "Start");
+        ButtonCreator players = new ButtonCreator(275, 350, 350, 100, Color.YELLOW, "2 Players");
+        ButtonCreator boosts = new ButtonCreator(250, 450, 400, 100, Color.GREEN, "Boosts On");
+        ButtonCreator difficulty = new ButtonCreator(230, 550, 450, 100, Color.MAGENTA, "Easy");
+        ButtonCreator back = new ButtonCreator(305, 650, 300, 100, Color.RED, "Back");
+        
+        // Initializes the title for settings
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setLayout(null);
+        JLabel img = new JLabel(new ImageIcon("SETTINGS.png"));
+        panel.add(img);
+        Dimension size = img.getMaximumSize();
+        img.setBounds(170, 40, size.width, size.height);
+        
+        // Adds title, buttons and makes frame visible
+        frame.add(start);
+        frame.add(players);
+        frame.add(img);
+        frame.add(boosts);
+        frame.add(back);
+        frame.add(difficulty);
+        frame.setVisible(true);
+        
+        
+        // Adds action listener for the back button
+        back.addActionListener( new ActionListener(){
+    
+            public void actionPerformed(ActionEvent e){
+		startMenu.run(w, h, name);
+        frame.dispose();
+        }
+});
+        
+        // Adds action listener for the boosts button
+        boosts.addActionListener( new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e){
+        if (boostBool == true) {
+        boosts.setText("Boosts Off");  
+        boostBool = false;
+        }
+        else {
+            boosts.setText("Boosts On");
+            boostBool = true;
+        }
+            }
+});
+            
+        // Adds action listener for the players button
+        players.addActionListener( new ActionListener() {
+            
+                public void actionPerformed(ActionEvent e){
+        if (numOfPlayersText == 0) {
+            players.setText("2 Players");
+            numOfPlayers = 2;
+            numOfPlayersText += 1;
+        }
+        
+        else if (numOfPlayersText == 1) {
+            players.setText("3 Players");
+            numOfPlayers = 3;
+            numOfPlayersText += 1;
+        }
+        
+        else if (numOfPlayersText == 2) {
+            players.setText("4 Players");
+            numOfPlayers = 4;
+            numOfPlayersText -= 2;
+        }
+                }
+});
+            
+        // Adds action listener for the difficulty button
+        difficulty.addActionListener( new ActionListener() {
+           
+           public void actionPerformed(ActionEvent e) {
+        if (difficultySpeed == 100) {
+            difficulty.setText("Intermediate");
+            difficultySpeed -= 30;
+            System.out.println(difficultySpeed);
+        }
+        
+        else if (difficultySpeed == 70) {
+            difficulty.setText("Expert");
+            difficultySpeed -= 35;
+            System.out.println(difficultySpeed);
+        }
+        
+        else if (difficultySpeed == 35) {
+            difficulty.setText("Easy");
+            difficultySpeed += 65;
+            System.out.println(difficultySpeed);
+        }
+    }
+
+});   
+            
+        // Adds action listener for the start button
+        start.addActionListener( new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e) {
+        gameGoing = true;
+		timer = false;
+		frame.dispose();
+    }
+    
+});
+
+    }
+    
+    /**
+     * Getter for number of players
+     */
+    public int getPlayers() {
+        return numOfPlayers;
+    }
+    
+    /**
+     * Getter for the boost boolean
+    */
+    public boolean getBoost() {
+        return boostBool;
+    }
+ 
+    /**
+     * Getter for difficulty
+     */
+    public int getDifficulty() {
+        return difficultySpeed;
+    }
+
+	/**
+     *Getter for getGameGoing
+     */
+    public boolean getGameGoing() {
+	    return gameGoing;
+    }
+
+    /**
+    * Getter for timer
+    */
+    public boolean getTimer() {
+	    return timer;
+	}
+    
+    /**
+    * Setter method for gameGoing
+    *
+    * @param setter passes in setter for gameGoing
+    */
+	public void setGameGoing(boolean setter) {
+	    gameGoing = setter;
+	}
+    
+}
