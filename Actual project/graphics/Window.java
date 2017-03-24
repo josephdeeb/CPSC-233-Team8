5d835d2468a612b1bd515496d559251426b2c463
josephdeeb <josephdbesenski@gmail.com>
2017-03-24 11:42:08 -0600

    Add files via upload

---
+++ Actual project/graphics/Window.java
@@ -0,0 +1,187 @@
+package graphics;
+import javax.swing.JFrame;
+
+import other.Main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Color;
+public class Window extends JFrame implements KeyListener {
+
+    /**
+     * Creates a window and adds a key listener for keyboard events.
+     *
+     * @param width Width of the window
+     * @param height Height of the window
+     * @param name Name of the window
+     */
+    public Window(int width, int height, String name) {
+
+        // Calls initialize function
+        initialize(width, height, name);
+
+        // Uses the key listener and key events to move the object on the grid
+        this.addKeyListener(this);
+        
+    }
+    
+    /**
+     * Determines the effects of the key event.
+     *
+     * @param event An event from a key press
+     */
+    public void keyPressed(KeyEvent event) {
+                
+        // PLAYER 1
+        if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+            try {
+                Main.getDriver(0).setDirection("right");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+            try {
+                Main.getDriver(0).setDirection("left");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+            try {
+                Main.getDriver(0).setDirection("down");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_UP) {
+            try {
+                Main.getDriver(0).setDirection("up");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+                
+                
+        // PLAYER 2
+        else if (event.getKeyCode() == KeyEvent.VK_D) {
+            try {
+                Main.getDriver(1).setDirection("right");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_A) {
+            try {
+                Main.getDriver(1).setDirection("left");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_S) {
+            try {
+                Main.getDriver(1).setDirection("down");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_W) {
+            try {
+                Main.getDriver(1).setDirection("up");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+                
+                
+        // PLAYER 3
+        else if (event.getKeyCode() == KeyEvent.VK_K) {
+            try {
+                Main.getDriver(2).setDirection("right");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_H) {
+            try {
+                Main.getDriver(2).setDirection("left");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_J) {
+            try {
+                Main.getDriver(2).setDirection("down");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_U) {
+            try {
+                Main.getDriver(2).setDirection("up");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+        
+        
+        // PLAYER 4
+        else if (event.getKeyCode() == KeyEvent.VK_NUMPAD6) {
+            try {
+                Main.getDriver(3).setDirection("right");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_NUMPAD4) {
+            try {
+                Main.getDriver(3).setDirection("left");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_NUMPAD5) {
+            try {
+                Main.getDriver(3).setDirection("down");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+
+        else if (event.getKeyCode() == KeyEvent.VK_NUMPAD8) {
+            try {
+                Main.getDriver(3).setDirection("up");
+            }
+            catch(ArrayIndexOutOfBoundsException exception) {}
+        }
+        
+    }
+
+    // Required for syntax but has no use in this game.
+    public void keyReleased(KeyEvent event) {}
+    public void keyTyped(KeyEvent event) {}
+
+    
+    /**
+     * Initializes and displays the window.
+     *
+     * @param width Width of the window
+     * @param height Height of the window
+     * @param name Name of the window
+     */
+    private void initialize(int width, int height, String name) {
+
+        // Makes it so that the X button, when pressed, exits the window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Sets window size to the specified width and height
+        setSize(width, height);
+
+        // Makes frame visible
+        getContentPane().setBackground(Color.BLACK);
+        setVisible(false);
+
+        // Sets window name
+        setName(name);
+        setTitle(name);
+
+    }
+
+}
+
---
+++ Actual project/other/Main.java
@@ -0,0 +1,158 @@
+package other;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.BorderFactory;
+
+import graphics.Cell;
+import graphics.Map;
+
+import java.lang.Thread;
+import java.util.Random;
+
+public class Main {
+
+    private static final int DEFAULT_WINDOW_WIDTH = 900;
+    private static final int DEFAULT_WINDOW_HEIGHT = 900;
+    private static final int DEFAULT_PLAYERS = 1;
+    private static final String DEFAULT_WINDOW_NAME = "T R O M";
+    private static Driver[] drivers;
+    private static StartMenu startMenu = new StartMenu();
+    private static Settings settings = new Settings();
+    private static Map map;
+
+    /**
+     * Main program function
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+		startMenu.run(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_NAME);
+		while (settings.getTimer()) {
+			stop(100);
+
+        }
+        
+        if (settings.getGameGoing()){
+			initializeGame(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_NAME, settings.getPlayers());
+		}
+		else{
+			finished();
+			
+        }
+    }
+    
+    /**
+     * Prints "Game is finished".
+     */
+	private static void finished(){
+		System.out.println("Game is finished");
+	}
+	
+	/**
+	 * Returns a driver from the main drivers array.
+	 * 
+	 * @param num Index number of wanted driver
+	 * @return Driver from drivers[] array
+	 */
+	public static Driver getDriver(int num) {
+	    try {
+	        return drivers[num];
+	    }
+	    catch (ArrayIndexOutOfBoundsException ex) {
+	        return drivers[0];
+	    }
+	    finally {}
+	}
+	
+    /**
+     * Returns a cell from the main cell array.
+     * 
+     * @param x X Coordinate of the wanted cell
+     * @param y Y Coordinate of the wanted cell
+     * @return Cell at (x,y) entry of labels array.
+     */
+    public static Cell getCell(int x, int y) {
+        return map.getCell(x, y);
+    }
+	
+	/**
+	 * Initializes and runs game
+	 * 
+	 * @param w Width of the window
+	 * @param h Height of the window
+	 * @param name Name of the window
+	 * @param players Amount of players
+	 */
+    private static void initializeGame(int w, int h, String name, int players) {
+        
+        if (players > 4)
+            players = 4;
+        else if (players < 1)
+            players = 1;
+        
+        drivers = new Driver[players];
+        for (int i = 0; i < players; i++) {
+            if (i == 0)
+                drivers[i] = new Driver(5, 5, Color.YELLOW, "down");
+            else if (i == 1)
+                drivers[i] = new Driver(44, 5, Color.RED, "left");
+            else if (i == 2)
+                drivers[i] = new Driver(44, 44, Color.CYAN, "up");
+            else if (i == 3)
+                drivers[i] = new Driver(5, 44, Color.MAGENTA, "right");
+        }
+        
+        map = new Map(w, h, name);
+        
+        if (settings.getBoost() == true) {
+            powerUp();
+        }
+        
+        int dead = 0;
+        // Main game loop
+        while (settings.getGameGoing()) {
+            dead = 0;
+            for (int i = 0; i < players; i++) {
+                if (drivers[i].getAlive() == true) {
+                    drivers[i].move();
+                }
+                else {
+                	dead++;
+                }
+            }
+            if (dead == (players - 1)) {
+                settings.setGameGoing(false);
+                for (int i = 0; i < players; i++) {
+                	System.out.println("Player "+ (i+1) + " FINAL SCORE: " + drivers[i].getScore());
+                }
+            }
+            map.repaintGame();
+            stop(settings.getDifficulty());
+        }
+        map.gameOver();
+        
+    }
+    // This method uses code copied from http://stackoverflow.com/questions/24104313/how-to-delay-in-java by Ann Ragg
+    private static void stop(int milliseconds) {
+        
+        try {
+            Thread.sleep(milliseconds);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    public static void powerUp() {
+        int randomX = new Random().nextInt(50);
+        int randomY = new Random().nextInt(50);
+        getCell(randomX, randomY).colorUpdate(Color.BLUE);
+        
+    }
+    
+    /**
+     * Sets the gameGoing loop to false.
+     */
+    public static void stopGame() {
+        settings.setGameGoing(false);
+    }
+}
