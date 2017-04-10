befd97175cae510ca75201243a015648c09e54cd
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 10:16:23 -0600

    Update Cell.java

--- ra/graphics/Cell.java
+++ ra/graphics/Cell.java
@@ -1,37 +1,44 @@
 package graphics;
 import javax.swing.JLabel;
-
 import javax.swing.BorderFactory;
-
 import java.awt.Color;
 
+/*
+ *@author Team 8
+ */
 public class Cell extends JLabel {
-
+    /**
+     *Used to set the current color
+     */
     private Color currentColor = null;
-
+    
+    /**
+     *Initializes the properties of the cell, which inherits properties from JLabel
+     *
+     *Sets the cells as black, opaque, textless cells
+     */
     public Cell() {
-
-        // Sets text to blank
         setText("");
-
-        // Sets background color of cell to black
         colorUpdate(Color.BLACK);
-
-        // Sets border color to yellow
-        //setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        // Makes the cell opaque (shows color)
         setOpaque(true);
     }
-
+    
+    /**
+     *Updates the current color to the argument given
+     *
+     *@param  color The new color to change the cells to
+     */
     public void colorUpdate(Color color) {
-
         setBackground(color);
         currentColor = color;
     }
 
+    /**
+     *Getter that returns the current color
+     *
+     *@return the current color
+     */
     public Color getColor() {
-
         return currentColor;
     }
 }
