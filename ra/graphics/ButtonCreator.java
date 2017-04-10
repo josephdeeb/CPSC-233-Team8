83f6b0158f2589be54606269e0b42a9956f54e82
jasonlyster <lysterjason@gmail.com>
2017-04-10 10:25:02 -0600

    Update ButtonCreator.java

--- ra/graphics/ButtonCreator.java
+++ ra/graphics/ButtonCreator.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 
 
 public class ButtonCreator extends JButton{	
+	
 	/**
      * Creates a button.
      *
@@ -17,6 +18,9 @@ public class ButtonCreator extends JButton{
      * @param height Determines how high the button is
      * @param color Sets the color of the foreground of the button
      * @param text Sets the name of the button
+     *
+     * @exception FontFormatException unreadable format sets font to Arial
+     * @exception IOException if the ttf file does not exists, sets font to Arial
      */
 	
     public ButtonCreator(int xCoordinate, int yCoordinate, int width, int height, Color color, String text){
