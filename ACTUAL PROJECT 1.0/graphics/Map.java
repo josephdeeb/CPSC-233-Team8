b7d239168bfbc7c60d4cd88edd3300f81134b778
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 12:41:29 -0600

    Update Map.java

--- ACTUAL PROJECT 1.0/graphics/Map.java
+++ ACTUAL PROJECT 1.0/graphics/Map.java
@@ -1,44 +1,90 @@
 package graphics;
 
-
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.Random;
+import javax.swing.JPanel;
 
 import other.Main;
 
-import javax.swing.JPanel;
-
 public class Map {
+    
+    /**
+     * Sets the name of our game that'll appear at the top window
+     */
     public static final String DEFAULT_WINDOW_NAME = "T R O M";
+    
+    /**
+     * The width of all of the windows
+     */
     public static final int DEFAULT_WINDOW_WIDTH = 900;
+    
+    /**
+     * The height of all of the windows
+     */
     public static final int DEFAULT_WINDOW_HEIGHT = 900;
+    
+    /**
+     * The number of cells in the window
+     */
     private Cell[][] labels = new Cell[50][50];
+    
+    /**
+     * Creates a window instance
+     */
     private Window window;
+    
+    /**
+     * Creates a JPanel called game
+     */
     private JPanel game;
+    
+    /**
+     * Creates a main instance
+     */
     private Main main;
     
+    /**
+     *Constructor with one argument that calls constructor with four arguments
+     *
+     *@param initMain Main object to initalize graphical game with
+     */
     public Map(Main initMain) {
         this(initMain, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, DEFAULT_WINDOW_NAME);
     }
     
+    /**
+     *Constructor with two argument that calls constructor with four arguments
+     *
+     *@param initMain Main object to initialize graphical game with
+     *@param n Name of game "TROM"
+     */
     public Map(Main initMain, String n) {
         this(initMain, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, n);
     }
     
+    /**
+     *Constructor with four arguments that will initialize the game
+     *
+     *@param initMain Main object of game
+     *@param width Width of window
+     *@param height Height of window
+     *@param n Name of game "TROM"
+     */
     public Map(Main initMain, int width, int height, String n) {
         main = initMain;
         initialize(width, height, n);
     }
     
-    public void disposeWindow() {
-        window.dispose();
-    }
-    
+    /**
+     *Creates the window and the cells
+     *
+     *@param width Width of window
+     *@param height Height of window
+     *@param name Name of game "TROM"
+     */
     private void initialize(int w, int h, String name) {
-        
         window = new Window(w, h, name, main);
-
         game = new JPanel(new GridLayout(50, 50));
         
         int x = 0;
@@ -58,10 +104,16 @@ public class Map {
         window.setVisible(true);
     }
     
+    /**
+     *Redraws game GUI
+     */
     public void repaintGame() {
         game.repaint();
     }
     
+    /**
+     *Draws the GAME OVER box
+     */
     public void gameOver() {
         drawBox(11,15);
         game(13, 17);
@@ -96,7 +148,12 @@ public class Map {
         }
     }
     
-    // P.S this ain't needed, draw line works for boxes if you specify the right coordinates
+    /**
+     *method to draw a box, namely the GAME OVER box
+     *
+     *@param x intial x coordinate
+     *@param y initial y coordinate
+     */
     private void drawBox(int x, int y){
         //starting x: 11 starting y: 15
         for(int a=y; a<34; a++){
