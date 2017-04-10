be6b8a1e8e3032ffbde810faed22f32cdd58ac65
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 13:46:19 -0600

    Update StartMenu.java

--- ACTUAL PROJECT 1.0/other/StartMenu.java
+++ ACTUAL PROJECT 1.0/other/StartMenu.java
@@ -1,12 +1,13 @@
 package other;
 import java.awt.*;
+import java.awt.event.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import graphics.ButtonCreator;
 import graphics.Window;
 
-import java.awt.event.*;
+
 
 /** 
  * This class creates a JFrame that displays the start
@@ -15,22 +16,44 @@ import java.awt.event.*;
  */
 public class StartMenu{
 	
+    /**
+     * Whether or not game is running
+     */
 	private boolean gameGoing = false;
 	
+    /**
+     * Timer for the thread
+     */
 	private boolean timer = true;
+    
+    /**
+     * Create a settings instance
+     */
 	private Settings settings;
 	
+    /**
+     * Constructor that initializes the settings menu
+     *
+     * @param initSettings changing our settings menu
+     */
 	public StartMenu(Settings initSettings) {
 	    settings = initSettings;
 	}
-	
+	 
+    /**
+     * Getter method for gamegoing
+     *
+     * @return returns state of gameGoing
+     */
 	public boolean getGameGoing() {
 	    return gameGoing;
 	}
 	
     /**
-    * Getter for timer
-    */
+     * Getter for timer
+     *
+     * @return returns state of timer
+     */
     public boolean getTimer() {
 	    return timer;
 	}
@@ -39,13 +62,17 @@ public class StartMenu{
      * Run class which creates every button and performs
      * action intended for each button. Also generates the
      * logo that is displayed at the top of the frame.
+     *
+     * @param w width of window to create
+     * @param h height of window to create
+     * @name name of our window (TROM)
      */
     public void run(int w,int h, String name){
 	    
-        // Initialize JFrame
         Window frame = new Window(w, h, name);
 		System.out.println("STARTMENU");
-        // Create Buttons
+        
+        // Create Inital Buttons
 	    ButtonCreator start = new ButtonCreator(350, 250, 200, 100, Color.CYAN, "Play");
         ButtonCreator instructions = new ButtonCreator(200, 375, 500, 100, Color.YELLOW, "Instructions");
         ButtonCreator credits = new ButtonCreator(300, 500, 300, 100, Color.MAGENTA, "Credits");
@@ -67,67 +94,57 @@ public class StartMenu{
         frame.add(img);
         frame.setVisible(true);
 		
-        // Adds action listen and switches game going and timer booleans to start the game
+        // start button action listener
         start.addActionListener( new ActionListener(){
-    
             public void actionPerformed(ActionEvent e){
-        System.out.println("START clicked");
-        settings.run(w, h, name);
-        frame.dispose();
-        }
-});
+                System.out.println("START clicked");
+                settings.run(w, h, name);
+                frame.dispose();
+            }
+        });
 
         credits.addActionListener( new ActionListener(){
-    
             public void actionPerformed(ActionEvent e){
-        System.out.println("credits clicked");
-        frame.dispose();
-        Window creditsFrame = new Window(w,h,name);
-        JPanel namesPanel = new JPanel();
-        JLabel textLabel = new JLabel("<html>This game was created by:<br><br> Denis Shevchenko<br><br> Joseph Besenski<br><br>Jason Lyster<br><br>Daniel Sohn<br><br> Kevin Vo </html>");
-        textLabel.setFont(new Font("Serif", Font.ITALIC, 30));
-        textLabel.setForeground(Color.WHITE);
-        namesPanel.add(textLabel);
-        creditsFrame.setSize(900,900);
-        creditsFrame.getContentPane().setBackground(Color.BLACK);
-        namesPanel.setBackground(Color.BLACK);
-        creditsFrame.setLayout(null);
-        namesPanel.setSize(400,600);
-        namesPanel.setLocation(100,100);
-        creditsFrame.add(namesPanel);
-        creditsFrame.setVisible(true);
-        namesPanel.setVisible(true);
-        textLabel.setVisible(true);
-        
-        ButtonCreator exitCredits = new ButtonCreator(350, 625, 200, 100, Color.RED, "Back");
-        creditsFrame.add(exitCredits);
-        
-        // Adds action listener for the exit credits button
-        exitCredits.addActionListener( new ActionListener(){
-    
-            public void actionPerformed(ActionEvent e){
-        System.out.println("EXITCREDITS clicked");
-		creditsFrame.dispose();
-        run(w, h, name);
-        }
-});
- 
-        }
-            
-        }
-);
-        // Adds action listener to exit the game when exit is clicked
+                System.out.println("credits clicked");
+                frame.dispose();
+                Window creditsFrame = new Window(w,h,name);
+                JPanel namesPanel = new JPanel();
+                JLabel textLabel = new JLabel("<html>This game was created by:<br><br> Denis Shevchenko<br><br> Joseph Besenski<br><br>Jason Lyster<br><br>Daniel Sohn<br><br> Kevin Vo </html>");
+                textLabel.setFont(new Font("Serif", Font.ITALIC, 30));
+                textLabel.setForeground(Color.WHITE);
+                namesPanel.add(textLabel);
+                creditsFrame.setSize(900,900);
+                creditsFrame.getContentPane().setBackground(Color.BLACK);
+                namesPanel.setBackground(Color.BLACK);
+                creditsFrame.setLayout(null);
+                namesPanel.setSize(400,600);
+                namesPanel.setLocation(100,100);
+                creditsFrame.add(namesPanel);
+                creditsFrame.setVisible(true);
+                namesPanel.setVisible(true);
+                textLabel.setVisible(true);
+
+                ButtonCreator exitCredits = new ButtonCreator(350, 625, 200, 100, Color.RED, "Back");
+                creditsFrame.add(exitCredits);
+                exitCredits.addActionListener( new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        System.out.println("EXITCREDITS clicked");
+                        creditsFrame.dispose();
+                        run(w, h, name);
+                    }
+                });
+            }
+        });
+
         exit.addActionListener( new ActionListener(){
-    
            public void actionPerformed(ActionEvent e){
-        System.out.println("EXIT clicked");
-		timer = false;
-		gameGoing = false;
-		frame.dispose();
-        }
-});
+                System.out.println("EXIT clicked");
+                timer = false;
+                gameGoing = false;
+                frame.dispose();
+            }
+        });
 
     }
 	  
 }
-
