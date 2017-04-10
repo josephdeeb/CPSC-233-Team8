cdb9451250afea826c4c5e3076b81e3455be0fb9
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 13:53:21 -0600

    Update Run.java

--- ACTUAL PROJECT 1.0/other/Run.java
+++ ACTUAL PROJECT 1.0/other/Run.java
@@ -1,15 +1,20 @@
 package other;
 
 public class Run {
+    
+    /**
+     * whether or not main is being run
+     */
     public static boolean running = true;
 
+    /**
+     * Main method that constantly creates a new main while the user wants it, until the exit game is pressed
+     */
     public static void main(String[] args) {
         Main main = null;
         while (running) {
             main = new Main();
             running = main.start();
         }
-
     }
-
 }
