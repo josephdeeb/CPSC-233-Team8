93643389d8e3323dee91e10f7b2c430b1bd26516
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 10:14:55 -0600

    Update Run.java

--- ra/other/Run.java
+++ ra/other/Run.java
@@ -1,7 +1,9 @@
 package other;
 
 public class Run {
-
+    /*
+     *Main method that runs start method from the main class
+     */
     public static void main(String[] args) {
         Main main = new Main();
         main.start();
