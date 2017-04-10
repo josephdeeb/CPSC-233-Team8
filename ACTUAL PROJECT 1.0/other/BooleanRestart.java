ac3ee6bcf310d9a96e48ef9111e627071100a841
DanSohn <chiefgustavo000@gmail.com>
2017-04-10 12:54:57 -0600

    Update BooleanRestart.java

--- ACTUAL PROJECT 1.0/other/BooleanRestart.java
+++ ACTUAL PROJECT 1.0/other/BooleanRestart.java
@@ -1,8 +1,19 @@
 package other;
 
 public class BooleanRestart {
+    
+    /**
+     * Condition of whether or not the game should be restarted
+     */
     public boolean returned = false;
+    
+    /**
+     * Sets the variable returned to the change
+     *
+     * @param change The new condition of whether the user wants to restart game or not
+     */
     public void setReturned(Boolean change) {
         returned = change;
     }
+    
 }
